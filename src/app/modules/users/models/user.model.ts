627b8a8296b1bc75cc7edd4c301c2b760a16aa77
jbuendia1y <jgamer669@gmail.com>
2022-11-06 14:02:52 -0500

    Creating models

---
+++ src/app/modules/users/models/user.model.ts
@@ -0,0 +1,21 @@
+export interface IUser {
+  id: string;
+  avatar?: string;
+  firstName: string;
+  lastName: string;
+
+  username: string;
+  email: string;
+  phone?: string;
+}
+
+export interface ICreateUser {
+  id: string;
+  avatar?: string;
+
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password: string;
+}
