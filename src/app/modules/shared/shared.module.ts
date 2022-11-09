dfbcf8646a63a38d893a77c2e2076c651a32903f
jbuendia1y <jgamer669@gmail.com>
2022-11-08 19:55:34 -0500

    componente header listo para implementar

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from 'src/app/components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [CommonModule, RouterModule, FontAwesomeModule],
-  exports: [RouterModule, FontAwesomeModule],
+  exports: [RouterModule, FontAwesomeModule, HeaderComponent],
 })
 export class SharedModule {}
