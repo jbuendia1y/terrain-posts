433797aa1cf8466548eccac7e07d3b60d78f98e9
jbuendia1y <jgamer669@gmail.com>
2022-11-17 18:20:05 -0500

    Servicio para almacenar en firebase storage

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -4,52 +4,78 @@ import {
   uploadBytes,
   ref,
   uploadString,
-  StorageReference,
+  list,
+  UploadMetadata,
+  ListOptions,
+  getDownloadURL,
 } from '@angular/fire/storage';
+import { from, map } from 'rxjs';
+
+interface Options {
+  path?: string;
+  metadata?: UploadMetadata;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class StorageService {
   private readonly uploadSource = 'uploads';
-  private uploadRef: StorageReference;
 
-  constructor(private storage: Storage) {
-    this.uploadRef = ref(this.storage, this.uploadSource);
-  }
+  constructor(private storage: Storage) {}
 
-  async uploadFromBytes(files: FileList | File[]) {
+  async uploadFromBytes(files: FileList | File[], options?: Options) {
+    const url = this.uploadSource.concat('/', options?.path?.concat('/') || '');
     const result = await Promise.all(
-      Array.from(files).map((f) => uploadBytes(this.uploadRef, f))
+      Array.from(files).map((f) => {
+        const r = ref(this.storage, url.concat(f.name));
+        return uploadBytes(r, f, options?.metadata);
+      })
     );
     return result;
   }
 
-  async uploadFromString(files: { value: string; name: string }[]) {
+  async uploadFromString(
+    files: { value: string; name: string }[],
+    options?: Options
+  ) {
+    const url = this.uploadSource.concat('/', options?.path?.concat('/') || '');
     const result = await Promise.all(
-      Array.from(files).map((f) =>
-        uploadString(this.uploadRef, f.value, 'base64url', {
-          customMetadata: {
-            name: f.name,
-          },
-        })
-      )
+      Array.from(files).map((f) => {
+        const r = ref(this.storage, url.concat(f.name));
+        return uploadString(r, f.value, 'base64url', options?.metadata);
+      })
     );
     return result;
   }
 
   async uploadFiles(
-    files: FileList | { value: string; name: string }[] | File[]
+    files: FileList | { value: string; name: string }[] | File[],
+    options?: Options
   ) {
+    console.log('UPLOADING ??');
     if (files instanceof FileList) {
-      console.log('files');
-      return await this.uploadFromBytes(files);
+      console.log('list files');
+      return await this.uploadFromBytes(files, options);
     } else if (Array.isArray(files) && (files[0] as any).value) {
       console.log('strings');
-      return await this.uploadFromString(files as any);
+      return await this.uploadFromString(files as any, options);
     } else if (Array.isArray(files) && (files[0] as any).text) {
-      console.log('files');
-      return await this.uploadFromBytes(files as File[]);
-    } else return [];
+      console.log('array files');
+      return await this.uploadFromBytes(files as File[], options);
+    } else {
+      console.log('F');
+      return [];
+    }
+  }
+
+  findAll(options?: ListOptions, ...path: string[]) {
+    const r = ref(this.storage, this.uploadSource.concat('/', path.join('/')));
+    return from(list(r, options));
+  }
+
+  getDownloadUrl(...path: string[]) {
+    const r = ref(this.storage, this.uploadSource.concat('/', path.join('/')));
+    return getDownloadURL(r);
   }
 }
