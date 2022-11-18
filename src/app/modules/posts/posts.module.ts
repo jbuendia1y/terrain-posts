14441fd7939d19b39597a455dc9162b7fe30fab5
jbuendia1y <jgamer669@gmail.com>
2022-11-18 09:10:52 -0500

    Creando rutas y añadiendo componentes

--- src/app/modules/posts/posts.module.ts
+++ src/app/modules/posts/posts.module.ts
@@ -1,12 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { PostsComponent } from './pages/posts/posts.component';
+import { PostCardComponent } from './components/post-card/post-card.component';
+import { PostsRoutingModule } from './posts-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import {
+  NgbCarouselModule,
+  NgbDropdownModule,
+  NgbPaginationModule,
+} from '@ng-bootstrap/ng-bootstrap';
+import { CreatePostFormComponent } from './components/create-post-form/create-post-form.component';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CreateFeatureFormComponent } from './components/create-feature-form/create-feature-form.component';
+import { IconPickerModule } from 'ngx-icon-picker';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PostsComponent,
+    PostCardComponent,
+    CreatePostFormComponent,
+    CreatePostComponent,
+    CreateFeatureFormComponent,
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NgbPaginationModule,
+    NgbCarouselModule,
+    NgbDropdownModule,
+    IconPickerModule,
+    SharedModule,
+    ReactiveFormsModule,
+    PostsRoutingModule,
+  ],
 })
-export class PostsModule { }
+export class PostsModule {}
